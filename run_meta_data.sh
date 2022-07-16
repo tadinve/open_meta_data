0f509deabde893f7e619d7f1881a49007904b9da
Venkatesh Tadinada <venkat@solivar.com>
2022-07-16 02:43:50 +0000

    first version

--- run_meta_data.sh
+++ run_meta_data.sh
@@ -1,7 +1,12 @@
-python3 -m venv env
-source env/bin/activate
-pip3 install openmetadata-ingestion[docker]
-metadata docker --start
-# http://localhost:8080  admin/admin
+# Instructions
+- python3 -m venv env
+- source env/bin/activate
+- pip3 install openmetadata-ingestion[docker]
+- metadata docker --start
 
-# view metadata http://localhost:8585
+# Airflow
+http://localhost:8080  admin/admin
+
+# view metadata 
+http://localhost:8585
+
