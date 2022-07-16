f855f7a2dc4fed202a3d8bb9fb2830365428a9d4
Venkatesh Tadinada <venkat@solivar.com>
2022-07-16 02:41:09 +0000

    first version

---
+++ run_meta_data.sh
@@ -0,0 +1,7 @@
+python3 -m venv env
+source env/bin/activate
+pip3 install openmetadata-ingestion[docker]
+metadata docker --start
+# http://localhost:8080  admin/admin
+
+# view metadata http://localhost:8585
